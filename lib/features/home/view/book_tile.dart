faaff8fba4435f6fac01435e8ad9759521fb1a38
egemenongel <egemenongel@hotmail.com>
2024-02-14 16:03:34 +0300

    refactor: Refactor code

--- lib/features/home/view/book_tile.dart
+++ lib/features/home/view/book_tile.dart
@@ -8,7 +8,11 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class BookTile extends ConsumerWidget {
-  const BookTile({super.key, required this.book, required this.index});
+  const BookTile({
+    super.key,
+    required this.book,
+    required this.index,
+  });
 
   final BookModel book;
   final int index;
@@ -25,112 +29,207 @@ class BookTile extends ConsumerWidget {
             .read(localBookListServiceProvider)
             .removeBookFromFavorites(index, book);
       },
-      child: SizedBox(
-        height: 170,
-        child: ValueListenableBuilder(
-          valueListenable:
-              ref.read(localBookListServiceProvider).getBooks().listenable(),
-          builder: (_, books, ___) => Container(
-            margin: const EdgeInsets.all(8),
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(
-                Radius.circular(12),
-              ),
-              border: Border.all(
-                width: 2,
-                color: books.values.contains(book) ? Colors.blue : Colors.white,
-              ),
+      child: _BookCard(book: book),
+    );
+  }
+}
+
+class _BookCard extends ConsumerWidget {
+  const _BookCard({
+    required this.book,
+  });
+
+  final BookModel book;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SizedBox(
+      height: 170,
+      child: ValueListenableBuilder(
+        valueListenable:
+            ref.read(localBookListServiceProvider).getBooks().listenable(),
+        builder: (_, books, ___) => Container(
+          margin: const EdgeInsets.all(8),
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(
+              Radius.circular(12),
             ),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: book.imageLinks == null
-                      ? const _NoImageWidget()
-                      : CachedNetworkImage(
-                          imageUrl: book.imageLinks!.thumbnail!,
-                          errorWidget: (_, __, ___) => const _NoImageWidget(),
-                          placeholder: (context, url) => const LoadingWidget(),
-                          fit: BoxFit.cover,
-                          height: 150,
-                        ),
-                ),
-                Expanded(
-                  flex: 5,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          book.title ?? 'Bilinmiyor',
-                          maxLines: 1,
-                          style: context.textTheme.headlineSmall,
-                        ),
-                        const Divider(),
-                        if (book.description != null)
-                          Text(
-                            book.authors != null
-                                ? 'Description: ${book.description}'
-                                : '',
-                            style: context.textTheme.bodyLarge,
-                            maxLines: 2,
-                          ),
-                        const SizedBox(height: 10),
-                        if (book.authors?.isNotEmpty ?? true)
-                          Text(
-                            book.authors != null
-                                ? 'Authors: ${book.authors!.join(',')}'
-                                : '',
-                            style: context.textTheme.bodyLarge,
-                            maxLines: 1,
-                          ),
-                        if (book.publisher != null)
-                          Text(
-                            'Publisher: ${book.publisher}',
-                            style: context.textTheme.bodyLarge,
-                            textAlign: TextAlign.left,
-                          ),
-                        const Spacer(),
-                        Row(
-                          children: [
-                            if (book.publishedDate != null) ...[
-                              Flexible(
-                                flex: 5,
-                                child: Text(
-                                  book.publishedDate != null
-                                      ? 'Published at: ${book.publishedDate}'
-                                      : '',
-                                  style: context.textTheme.bodySmall,
-                                  maxLines: 1,
-                                ),
-                              ),
-                              const Spacer(),
-                            ],
-                            if (book.pageCount != null) ...[
-                              Flexible(
-                                flex: 2,
-                                child: Text(
-                                  book.pageCount != null
-                                      ? 'Pages: ${book.pageCount}'
-                                      : '',
-                                  style: context.textTheme.bodySmall,
-                                  maxLines: 1,
-                                ),
-                              ),
-                            ]
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+            border: Border.all(
+              width: 2,
+              color: books.values.contains(book) ? Colors.blue : Colors.white,
             ),
+          ),
+          child: Row(
+            children: [
+              _BookImage(book: book),
+              _BookInfo(book: book),
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _BookImage extends StatelessWidget {
+  const _BookImage({
+    required this.book,
+  });
+
+  final BookModel book;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: book.imageLinks == null
+          ? const _NoImageWidget()
+          : CachedNetworkImage(
+              imageUrl: book.imageLinks!.thumbnail!,
+              errorWidget: (_, __, ___) => const _NoImageWidget(),
+              placeholder: (context, url) => const LoadingWidget(),
+              fit: BoxFit.cover,
+              height: 150,
+            ),
+    );
+  }
+}
+
+class _BookInfo extends StatelessWidget {
+  const _BookInfo({
+    required this.book,
+  });
+
+  final BookModel book;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 5,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _Title(book: book),
+            const Divider(),
+            if (book.description != null) _Description(book: book),
+            const SizedBox(height: 10),
+            if (book.authors?.isNotEmpty ?? true) _Authors(book: book),
+            if (book.publisher != null) _Publisher(book: book),
+            const Spacer(),
+            _BottomPart(book: book),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title({
+    required this.book,
+  });
+
+  final BookModel book;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      book.title ?? 'Bilinmiyor',
+      maxLines: 1,
+      style: context.textTheme.headlineSmall,
+    );
+  }
+}
+
+class _Description extends StatelessWidget {
+  const _Description({
+    required this.book,
+  });
+
+  final BookModel book;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      book.description != null ? 'Description: ${book.description}' : '',
+      style: context.textTheme.bodyLarge,
+      maxLines: 2,
+    );
+  }
+}
+
+class _Authors extends StatelessWidget {
+  const _Authors({
+    required this.book,
+  });
+
+  final BookModel book;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      book.authors != null ? 'Authors: ${book.authors!.join(',')}' : '',
+      style: context.textTheme.bodyLarge,
+      maxLines: 1,
+    );
+  }
+}
+
+class _Publisher extends StatelessWidget {
+  const _Publisher({
+    required this.book,
+  });
+
+  final BookModel book;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Publisher: ${book.publisher}',
+      style: context.textTheme.bodyLarge,
+      textAlign: TextAlign.left,
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({
+    required this.book,
+  });
+
+  final BookModel book;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        if (book.publishedDate != null) ...[
+          Flexible(
+            flex: 5,
+            child: Text(
+              book.publishedDate != null
+                  ? 'Published at: ${book.publishedDate}'
+                  : '',
+              style: context.textTheme.bodySmall,
+              maxLines: 1,
+            ),
+          ),
+          const Spacer(),
+        ],
+        if (book.pageCount != null) ...[
+          Flexible(
+            flex: 2,
+            child: Text(
+              book.pageCount != null ? 'Pages: ${book.pageCount}' : '',
+              style: context.textTheme.bodySmall,
+              maxLines: 1,
+            ),
+          ),
+        ]
+      ],
     );
   }
 }
