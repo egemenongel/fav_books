a7c8c97d69211afea1cefc2ce719156945e2eda8
egemenongel <egemenongel@hotmail.com>
2024-02-14 13:23:11 +0300

    feat: Create book list viewmodel

---
+++ lib/features/home/viewmodel/book_list_viewmodel.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:fav_books/features/home/service/api_book_list_service.dart';
+import 'package:fav_books/features/models/volume_model.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class BookListViewmodel extends AutoDisposeAsyncNotifier<VolumeModel?> {
+  @override
+  FutureOr<VolumeModel?> build() {
+    return null;
+  }
+
+  Future<void> search(String query) async {
+    final service = ref.read(apiBookListServiceProvider);
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(() => service.search(query));
+  }
+}
